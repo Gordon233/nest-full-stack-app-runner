e591a2d7b51ef8e53f9b698c0162eda236bf79fc
Gordon Zhang <Gordon Zhang>
2025-07-02 00:45:20 +0800

    update

--- api/src/coffees/coffees.service.ts
+++ api/src/coffees/coffees.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './coffee.entity';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Injectable()
 export class CoffeesService {
@@ -16,8 +17,8 @@ export class CoffeesService {
     return this.coffees;
   }
 
-  findOne(id: string) {
-    const coffee = this.coffees.find((coffee) => coffee.id === +id);
+  findOne(id: number) {
+    const coffee = this.coffees.find((coffee) => coffee.id === id);
     if (!coffee) {
       throw new NotFoundException(`Coffee #${id} not found`);
     }
@@ -28,14 +29,14 @@ export class CoffeesService {
     return this.coffees.push(createCoffeeDto as Coffee);
   }
 
-  update(id: string, updateCoffeeDto: unknown) {
+  update(id: number, updateCoffeeDto: UpdateCoffeeDto) {
     const coffee = this.findOne(id);
     if (coffee) {
     }
     return coffee;
   }
 
-  remove(id: string) {
+  remove(id: number) {
     const coffeeIndex = this.coffees.findIndex((coffee) => coffee.id === +id);
     if (coffeeIndex >= 0) {
       this.coffees.splice(coffeeIndex, 1);
