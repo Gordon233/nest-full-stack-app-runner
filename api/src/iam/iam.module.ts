93287a9300d18dd9d3322f978819b19285d836c7
Gordon Zhang <Gordon Zhang>
2025-07-17 23:00:00 +0800

    jwt

--- api/src/iam/iam.module.ts
+++ api/src/iam/iam.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
 import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
+import { AuthenticationController } from './authentication/authentication.controller';
+import { AuthenticationService } from './authentication/authentication.service';
+import { JwtModule } from '@nestjs/jwt';
+import jwtConfig from './authentication/config/jwt.config';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
   providers: [
     BcryptService,
     {
       provide: HashingService,
       useClass: BcryptService,
     },
+    AuthenticationService,
   ],
+  controllers: [AuthenticationController],
 })
 export class IamModule {}
