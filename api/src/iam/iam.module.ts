24ce34cf09759160ea04b8182c223ac6763f2e01
Gordon Zhang <Gordon Zhang>
2025-07-18 23:39:51 +0800

    feat(iam): register JWT config with ConfigModule
    - Add ConfigModule import to IAM module
    - Register jwtConfig with ConfigModule.forFeature() for proper configuration management

--- api/src/iam/iam.module.ts
+++ api/src/iam/iam.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { User } from '../users/entities/user.entity';
 import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
@@ -11,6 +12,7 @@ import jwtConfig from './authentication/config/jwt.config';
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
+    ConfigModule.forFeature(jwtConfig),
     JwtModule.registerAsync(jwtConfig.asProvider()),
   ],
   providers: [
